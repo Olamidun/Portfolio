0de0348fea1367fcf05308f865737010af776914
Olamidun <kolapoolamidun@gmail.com>
2020-07-14 18:00:07 +0100

    added animation on scroll

--- resources/js/portfolio.js
+++ resources/js/portfolio.js
@@ -1,5 +1,9 @@
 // ANIMATE ON SCROLL
-
-$('.js--wp-1').waypoint(function(direction){
-	$('.js--wp-1').addClass('animated fadeIn');
-})
+$(Document).ready(function(){
+	$('p').click(function(){
+		$(this).css('background-color', '#ff0000');
+	})
+	$('.js--wp-1').waypoint(function(direction){
+		$('.js--wp-1').addClass('animated fadeIn');
+	})
+});
