c68deae8c4d295838f19616da90b3b235c697aa0
Olamidun <kolapoolamidun@gmail.com>
2020-07-25 15:53:04 +0100

    included pictures for projects section

--- resources/js/portfolio.js
+++ resources/js/portfolio.js
@@ -1,9 +1,12 @@
-// ANIMATE ON SCROLL
-$(Document).ready(function(){
-	$('p').click(function(){
-		$(this).css('background-color', '#ff0000');
-	})
-	$('.js--wp-1').waypoint(function(direction){
-		$('.js--wp-1').addClass('animated fadeIn');
-	})
-});
+// CONSTRUCTORS
+
+// function Person() {
+// 	this.name = 'Brad';
+// }
+
+// // instantiate the person object
+
+// const brad = new Person()
+// console.log(brad);
+
+console.log('Hello')
